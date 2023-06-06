e74ca117d630b3e7e5b5b95332fcf9c83252cbec
red_ant <mugishathierry65@gmail.com>
2023-06-06 17:57:00 +0200

    project initials

---
+++ src/main/java/com/r_e_d_ant/contentcalendar/Application.java
@@ -0,0 +1,13 @@
+package com.r_e_d_ant.contentcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/r_e_d_ant/contentcalendar/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r_e_d_ant.contentcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
